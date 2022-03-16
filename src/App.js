f8e11305b8c6f05cd1c8a8f8c3d09bc77c09fd6b
ducngo89 <ducngo89@gmail.com>
2022-03-16 22:42:12 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  console.debug("[settings-client] loaded");
+  return (
+    <div id="Settings-Client">
+      <p>This is the settings-client</p>
+    </div>
+  );
+}
